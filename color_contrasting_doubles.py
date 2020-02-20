4b739360bebd9efa2d633c4b875330319dccdb3e
William <baiweilian@gmail.com>
2020-02-19 19:23:43 -0600

    Added list of color-contrasting_doubles

---
+++ color_contrasting_doubles.py
@@ -0,0 +1,26 @@
+from astroplan import FixedTarget
+
+# From skyandtelescope.com/
+#       observing/colored-double-stars-real-and-imagined/
+eta_cassiopeiae = FixedTarget.from_name('eta Cas')
+one_arietis = FixedTarget.from_name('1 Ari')
+gamma_andromedae = FixedTarget.from_name('gamma_and')
+iota_trianguli = FixedTarget.from_name('iot Tri')
+eta_persei = FixedTarget.from_name('eta Per')
+thirtytwo_eridani = FixedTarget.from_name('32 Eri')
+rho_orionis = FixedTarget.from_name('rho Ori')
+fourteen_aurigae = FixedTarget.from_name('14 Aur')
+iota_orionis = FixedTarget.from_name('iot Ori')
+gamma_leporis = FixedTarget.from_name('gam Lep')
+winter_albireo = FixedTarget.from_name('145 CMa')
+iota_cancri = FixedTarget.from_name('iot Cnc')
+twentyfour_comae_berenices = FixedTarget.from_name('24 Com')
+xi_bootes = FixedTarget.from_name('xi Boo')
+alpha_herculis = FixedTarget.from_name('alp Her')
+ninetyfive_herculis = FixedTarget.from_name('95 Her')
+zeta_lyrae = FixedTarget.from_name('zet Lyr')
+albireo = FixedTarget.from_name('Albireo')
+thirtyone_cygni = FixedTarget.from_name('31 Cyg')
+beta_capricorni = FixedTarget.from_name('bet Cap')
+gamma_delphini = FixedTarget.from_name('gam Del')
+delta_cephei = FixedTarget.from_name('del Cep')
